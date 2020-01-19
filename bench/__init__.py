24fa122e7f36c02df1d1cd98205ad3cd2eaa3819
Paul Skopnik <paul@skopnik.me>
2020-01-19 19:05:48 +0100

    Extend benchmark suite
     * CSV output
     * BenchmarkSpecification: Additional parameters controlling
       benchmark execution can be passed to @bench() decorator

--- bench/__init__.py
+++ bench/__init__.py
@@ -2,7 +2,9 @@ import functools
 import time
 import itertools
 import gc
-from typing import Any, Callable, Iterator, List, Optional, Tuple, Union
+import sys
+import csv
+from typing import Any, Callable, cast, Iterable, Iterator, List, Optional, Tuple, Union
 
 
 def _double(start: int=1) -> Iterator[int]:
@@ -33,23 +35,6 @@ def _double_human(start: int=1) -> Iterator[int]:
 		for fact in 1, 2, 5:
 			yield fact * current
 		current *= 10
-
-
-class Benchmarker(object):
-	def auto_bench_func(self, f: 'BenchmarkFunction') -> 'BenchTimer.Timing':
-		for n in _double_human():
-			timing = self._bench_func(f, n)
-			if timing.duration > 0.2:
-				return timing
-
-		return BenchTimer.Timing(0, 0.0, 0.0)
-
-	def _bench_func(self, f: 'BenchmarkFunction', n: int) -> 'BenchTimer.Timing':
-		b = BenchTimer(n, time.perf_counter)
-		gc.collect()
-		f(b)
-		gc.collect()
-		return b.collect_timing()
 
 
 class BenchTimer(object):
@@ -106,6 +91,20 @@ class BenchTimer(object):
 			self._n: int = n
 			self._primary: float = primary
 			self._offset: Optional[float] = offset
+
+		def __add__(self, other: 'BenchTimer.Timing') -> 'BenchTimer.Timing':
+			return BenchTimer.Timing(
+				self._n + other._n,
+				self._primary + other._primary,
+				self.offset + other.offset, # non existing offset is converted to 0.0
+			)
+
+		def __sub__(self, other: 'BenchTimer.Timing') -> 'BenchTimer.Timing':
+			return BenchTimer.Timing(
+				self._n - other._n,
+				self._primary - other._primary,
+				self.offset - other.offset, # non existing offset is converted to 0.0
+			)
 
 		@property
 		def n(self) -> int:
@@ -185,52 +184,187 @@ class BenchTimer(object):
 
 BenchmarkFunction = Callable[[BenchTimer], None]
 
-benchmark_functions: List[BenchmarkFunction] = []
-benchmark_functions_only: bool = False
 
-def bench() -> Callable[[BenchmarkFunction], BenchmarkFunction]:
-	def inner(f: BenchmarkFunction) -> BenchmarkFunction:
-		global benchmark_functions, benchmark_functions_only
-		if not benchmark_functions_only:
-			benchmark_functions.append(f)
-		return f
+class BenchmarkSpecification(object):
+	def __init__(self,
+		name: str,
+		function: BenchmarkFunction,
+		min_n: int = 1,
+		max_n: int = 0,
+		min_repeats: int = 1,
+	) -> None:
+		self._name = name
+		self._function: BenchmarkFunction = function
+		self._min_n: int = min_n
+		self._max_n: int = max_n
+		self._min_repeats: int = min_repeats
 
-	return inner
+	@property
+	def name(self) -> str:
+		return self._name
+
+	@property
+	def function(self) -> BenchmarkFunction:
+		return self._function
+
+	@property
+	def min_n(self) -> int:
+		return self._min_n
+
+	@property
+	def max_n(self) -> int:
+		return self._max_n
+
+	@property
+	def min_repeats(self) -> int:
+		return self._min_repeats
 
 
-def _bench_only() -> Callable[[BenchmarkFunction], BenchmarkFunction]:
-	def inner(f: BenchmarkFunction) -> BenchmarkFunction:
-		global benchmark_functions, benchmark_functions_only
-		if benchmark_functions_only:
+class BenchmarkRegistry(object):
+	def __init__(self) -> None:
+		self._specifications: List[BenchmarkSpecification] = []
+		self._only_active: bool = False
+
+	def __iter__(self) -> Iterator[BenchmarkSpecification]:
+		return iter(self._specifications)
+
+	def add(self, spec: BenchmarkSpecification, only: bool=False) -> None:
+		if only and self._only_active:
 			raise Exception(
 				"multiple benchmark functions marked as only function",
-				benchmark_functions[0].__name__,
-				benchmark_functions[0].__name__,
+				self._specifications[0].name,
+				spec.name,
 			)
-		benchmark_functions = [f]
-		benchmark_functions_only = True
-		return f
 
-	return inner
+		if only:
+			self._specifications = [spec]
+		elif not self._only_active:
+			self._specifications.append(spec)
 
-bench.only = _bench_only
 
-def _bench_skip() -> Callable[[BenchmarkFunction], BenchmarkFunction]:
-	return lambda f: f
+class BenchDecorator(object):
+	def __init__(self, default_registry: BenchmarkRegistry) -> None:
+		self._default_registry: BenchmarkRegistry = default_registry
 
-bench.skip = _bench_skip
+	def __call__(
+		self,
+		name: Optional[str] = None,
+		registry: Optional[BenchmarkRegistry] = None,
+		min_n: int = 1,
+		max_n: int = 0,
+		min_repeats: int = 1,
+	) -> Callable[[BenchmarkFunction], BenchmarkFunction]:
+		def inner(f: BenchmarkFunction) -> BenchmarkFunction:
+			name_i = name or f.__name__
+			registry_i = registry or self._default_registry
+
+			registry_i.add(
+				BenchmarkSpecification(name_i, f, min_n=min_n, max_n=max_n, min_repeats=min_repeats),
+			)
+
+			return f
+
+		return inner
+
+	def only(
+		self,
+		name: Optional[str] = None,
+		registry: Optional[BenchmarkRegistry] = None,
+		min_n: int = 1,
+		max_n: int = 0,
+		min_repeats: int = 1,
+	) -> Callable[[BenchmarkFunction], BenchmarkFunction]:
+		def inner(f: BenchmarkFunction) -> BenchmarkFunction:
+			name_i = name or f.__name__
+			registry_i = registry or self._default_registry
+
+			registry_i.add(
+				BenchmarkSpecification(name_i, f, min_n=min_n, max_n=max_n, min_repeats=min_repeats),
+				only=True,
+			)
+
+			return f
+
+		return inner
+
+	def skip(
+		self,
+		name: Optional[str]=None,
+		registry: Optional[BenchmarkRegistry]=None
+	) -> Callable[[BenchmarkFunction], BenchmarkFunction]:
+		return lambda f: f
+
+
+class Benchmarker(object):
+	def auto_bench(self, spec: BenchmarkSpecification) -> Iterable[BenchTimer.Timing]:
+		n, _ = self._find_suitable_n(spec)
+		# TODO pass in already known timing
+		return self._full_eval(spec, n)
+
+	def _find_suitable_n(self, spec: BenchmarkSpecification) -> Tuple[int, BenchTimer.Timing]:
+		for n in _double_human(start=spec.min_n):
+			timing = self._bench_spec_n(spec, n)
+			if timing.duration > 0.2:
+				return n, timing
+
+		raise Exception("no suitable n found") # TODO
+
+	def _full_eval(self, spec: BenchmarkSpecification, n: int) -> Iterable[BenchTimer.Timing]:
+		for _ in range(spec.min_repeats):
+			yield self._bench_spec_n(spec, n)
+
+	def _bench_spec_n(self, spec: BenchmarkSpecification, n: int) -> BenchTimer.Timing:
+		k_it: Iterator[int] = itertools.repeat(n, 1)
+		if spec.max_n > 0 and n > spec.max_n:
+			k_it = itertools.repeat(spec.max_n, n // spec.max_n)
+			if n % spec.max_n > spec.min_n:
+				k_it = itertools.chain(k_it, itertools.repeat(n % spec.max_n, 1))
+
+		return sum(map(functools.partial(self._bench_func, spec.function), k_it), BenchTimer.Timing(0, 0.0, 0.0))
+
+	def _bench_func(self, function: BenchmarkFunction, n: int) -> BenchTimer.Timing:
+		b = BenchTimer(n, time.perf_counter)
+		gc.collect()
+		function(b)
+		gc.collect()
+		return b.collect_timing()
+
+
+default_registry = BenchmarkRegistry()
+bench = BenchDecorator(default_registry)
 
 def main_bench_registered() -> None:
 	b = Benchmarker()
 
 	res: List[Tuple[str, BenchTimer.Timing]] = []
 
-	for f in benchmark_functions:
-		timing = b.auto_bench_func(f)
-		res.append((f.__name__, (timing)))
+	w = csv.writer(sys.stdout)
+	w.writerow([
+		'name',
+		'n',
+		'duration',
+		'primary',
+		'offset',
+		'duration_per_execution',
+		'primary_per_execution',
+		'offset_per_execution',
+	])
 
-		print("{0} dur={1.duration_per_execution} n={1.n} total={1.primary} off={1.offset}".format(f.__name__, timing))
-		# print(*res[-1])
+	for spec in default_registry:
+		for timing in b.auto_bench(spec):
+			res.append((spec.name, timing))
+
+			print("{0} dur={1.duration_per_execution} n={1.n} total={1.primary} off={1.offset}".format(spec.name, timing), file=sys.stderr)
+			w.writerow([
+				spec.name,
+				timing.n,
+				timing.duration,
+				timing.primary,
+				timing.offset,
+				timing.duration_per_execution,
+				timing.primary_per_execution,
+				timing.offset_per_execution,
+			])
 
 # if __name__ == '__main__':
 # 	main()
